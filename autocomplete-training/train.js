df39129a70e395425a427682e2a2339d36f23dfe
Nick Visalli <nvisal1@students.towson.edu>
2020-01-22 09:16:23 -0500

    Create script for training the autocomplete algorithm

---
+++ autocomplete-training/train.js
@@ -0,0 +1,29 @@
+let englishStopWords = require('stopwords').english;
+const axios = require('axios');
+
+englishStopWords = englishStopWords.join(' ');
+
+const instance = axios.create({ baseURL: 'http://localhost:5000' });
+ 
+
+instance.post('/train', {
+    passage: englishStopWords,
+  })
+  .then(function (response) {
+    if (response.status === 204) {
+        console.log('Success');
+    } else {
+        console.error(`Unexpected Response: ${ response.status }`);
+    }
+  })
+  .catch(function (error) {
+    if (error.response) {
+        console.error(`Error Status: ${ error.response.status }.`);
+    } else {
+        console.error('Could not establish connection to the Autocomplete Service. Please ensure that the Autocomplete Service is running.');
+    }
+   
+  });
+
+
+
